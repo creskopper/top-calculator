ddef2ff0db08da515f367339e80fe3bade9b160f
Cresk <creskopper@gmail.com>
2022-09-24 10:36:14 +0100

    Add operate function to calculate

--- script.js
+++ script.js
@@ -14,3 +14,18 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b;
 }
+
+function operate(x, y, operator) {
+    switch (operator) {
+        case "+":
+            return add(x, y);
+        case "-":
+            return subtract(x, y);
+        case "x":
+            return multiply(x, y);
+        case "÷":
+            return divide(x, y);
+  
+        default: break;
+    }
+}
