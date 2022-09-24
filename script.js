98c798e8e1a786d766702500a0f67225a403d1fc
Cresk <creskopper@gmail.com>
2022-09-24 11:40:51 +0100

    Populate display when numbers clicked

--- script.js
+++ script.js
@@ -29,3 +29,22 @@ function operate(x, y, operator) {
         default: break;
     }
 }
+
+const display = document.querySelector(".display");
+const btn = document.querySelectorAll(".btn");
+const listOfBtn = Array.from(btn);
+let displayValue = "";
+let firstNumber = null;
+let secondNumber = null;
+let operator = null;
+let solution = null;
+let operatorClicked = false;
+
+listOfBtn.map(btn => {
+    btn.addEventListener("click", () => {
+        if (btn.classList.value == "btn digit") {
+            displayValue += btn.textContent;
+            display.textContent = displayValue;
+        }
+    })
+})
